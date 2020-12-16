c31114c27e2518bb1da9add7ee3638804461c6ae
math-yew <math.u@hotmail.com>
2020-12-16 15:46:49 -0700

    remote data

---
+++ remoteData.js
@@ -0,0 +1,32 @@
+// import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+
+console.log("yogi");
+
+
+let url = "https://docs.google.com/document/d/e/2PACX-1vRT3ZTtpTzF7W65QKPJnGbqW0AVKW4L9RbaSm8AHcdHHozlNdBF5F9sP1DP2_qh7c0QtXZ_LWfSzTcB/pub";
+// url = "https://jsonplaceholder.typicode.com/posts";
+url = "https://drive.google.com/file/d/1AKze0eXxAew6wxceTX2xT8LyGUaDe7dG/view?usp=sharing";
+
+// fetch(url).then(function (response) {
+// 	return response.json();
+// }).then(function (data) {
+// 	console.log(data);
+// }).catch(function (err) {
+// 	console.warn('Something went wrong.', err);
+// });
+
+
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+/*
+const octokit = new Octokit({ auth: `personal-access-token123` });
+
+async function asyncCall() {
+  const response = await octokit.request("GET /orgs/{org}/repos", {
+    org: "octokit",
+    type: "private",
+  });
+console.log("response" + response);
+}
+
+asyncCall();
+*/
